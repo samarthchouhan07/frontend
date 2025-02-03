b894521dff9eb6b318aa92c8160141f6b9b529e5
samarthchouhan07 <samarthchouhan29544@gmail.com>
2025-02-03 22:47:27 +0530

    courseid page started

--- app/teacher/courses/[courseId]/page.tsx
+++ app/teacher/courses/[courseId]/page.tsx
@@ -1,13 +1,35 @@
 "use client";
 import { Header } from "@/components/header";
+import { Progress } from "@/components/ui/progress";
+import { Settings } from "lucide-react";
+import { useState } from "react";
 
-export default function CoursesIdPage() {
+export default function CoursesIdPage({
+  params,
+}: {
+  params: {
+    courseId: string;
+  };
+}) {
+  const [filledFields, setFilledFields] = useState(0);
+  const totalFields = 5;
   return (
     <div className="flex min-h-screen">
       <div className="flex-1">
         <Header />
-        <main className="p-6">
-            CourseId page
+        <main className="p-6 max-w-2xl mx-auto space-y-4">
+          <h1 className="text-2xl font-bold">Course Setup</h1>
+          <div className="text-gray-600 text-sm">
+            Complete all the fields ({filledFields}/{totalFields})
+          </div>
+          <Progress
+            value={(filledFields / totalFields) * 100}
+            className="h-2"
+          />
+          <div className="flex items-center gap-2 mt-4">
+            <Settings className="w-6 h-6 text-blue=500" />
+            <p className="text-lg font-medium">Customize your course</p>
+          </div>
         </main>
       </div>
     </div>
